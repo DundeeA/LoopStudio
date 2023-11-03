72a5d1d84611ca173ad209d3968de5c2270beb65
DundeeA <dundeedevelops@gmail.com>
2023-11-03 05:55:05 -0500

    Added card backgrounds

--- src/components/creations/Creations.tsx
+++ src/components/creations/Creations.tsx
@@ -5,21 +5,21 @@ function Creations() {
     <div className="mt-20 pt-1 flex flex-col items-center justify-center">
       <h2 className="text-center font-thin  text-[2.3rem] mb-10">OUR CREATIONS</h2>
 
-      <CreationCard image={"/image-deep-earth.jpg"} title="DEEP EARTH" />
+      <CreationCard image="/image-deep-earth.jpg" title="DEEP EARTH" />
 
-      <CreationCard image={"url('/image-night-arcade.jpg')"} title="NIGHT ARCADE" />
+      <CreationCard image="/image-night-arcade.jpg" title="NIGHT ARCADE" />
 
-      <CreationCard image={"url('/image-soccer-team.jpg')"} title="SOCCER TEAM VR" />
+      <CreationCard image="/image-soccer-team.jpg" title="SOCCER TEAM VR" />
 
-      <CreationCard image={"/image-grid.jpg)"} title="THE GRID" />
+      <CreationCard image="/image-grid.jpg" title="THE GRID" />
 
-      <CreationCard image={"url('/image-from-above.jpg')"} title="FROM UP ABOVE VR" />
+      <CreationCard image="/image-from-above.jpg" title="FROM UP ABOVE VR" />
 
-      <CreationCard image={"url('/image-pocket-borealis.jpg')"} title="POCKET BOREALIS" />
+      <CreationCard image="/image-pocket-borealis.jpg" title="POCKET BOREALIS" />
 
-      <CreationCard image={"url('/image-curiosity.jpg')"} title="THE CURIOSITY" />
+      <CreationCard image="/image-curiosity.jpg" title="THE CURIOSITY" />
 
-      <CreationCard image={"url('/image-fisheye.jpg')"} title="MAKE IT FISHEYE" />
+      <CreationCard image="/image-fisheye.jpg" title="MAKE IT FISHEYE" />
     </div>
   )
 }
--- src/components/creations/creation-card/CreationCard.tsx
+++ src/components/creations/creation-card/CreationCard.tsx
@@ -6,7 +6,8 @@ interface props {
 
 const CreationCard: React.FC<props> = ({ image, title }) => {
   return (
-    <div className={`w-[87vw] h-[18vh] bg-[url('/image-deep-earth.jpg')] mb-6`}>
+    <div className={`w-[87vw] h-[18vh] mb-6 relative`}>
+      <img className="absolute -z-10" src={image} alt="" />
       <h3 className="text-[1.6rem] font-thin w-[32vw] mt-12 ml-5 leading-6 text-white">{title}</h3>
     </div>
   )
