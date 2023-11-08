3c8975e1ced1072bf225449e2e030331e79cd3e1
DundeeA <dundeedevelops@gmail.com>
2023-11-08 02:44:44 -0600

    Fixed creation section image routes

--- src/components/creations/Creations.tsx
+++ src/components/creations/Creations.tsx
@@ -5,22 +5,16 @@ function Creations() {
     <div className="mt-20 pt-1 flex flex-col items-center w-full">
       <h2 className="text-center font-thin  text-[2.3rem] mb-10">OUR CREATIONS</h2>
 
-      <div className=" grid grid-cols-1  md:grid-cols-4       w-[89vw] justify-center">
-        <CreationCard image="/image-deep-earth.jpg" title="DEEP EARTH" />
+      <div className=" grid grid-cols-1  md:grid-cols-4  md:w-[77.5vw]    w-[89vw] justify-center">
+        <CreationCard styles="bg-smEarth md:bg-lgEarth" title="DEEP EARTH" />
+        <CreationCard styles="bg-smArcade md:bg-lgArcade" title="NIGHT ARCADE" />
+        <CreationCard styles="bg-smSoccer md:bg-lgSoccer" title="SOCCER TEAM VR" />
+        <CreationCard styles="bg-smGrid md:bg-lgGrid" title="THE GRID" />
 
-        <CreationCard image="/image-night-arcade.jpg" title="NIGHT ARCADE" />
-
-        <CreationCard image="/image-soccer-team.jpg" title="SOCCER TEAM VR" />
-
-        <CreationCard image="/image-grid.jpg" title="THE GRID" />
-
-        <CreationCard image="/image-from-above.jpg" title="FROM UP ABOVE VR" />
-
-        <CreationCard image="/image-pocket-borealis.jpg" title="POCKET BOREALIS" />
-
-        <CreationCard image="/image-curiosity.jpg" title="THE CURIOSITY" />
-
-        <CreationCard image="/image-fisheye.jpg" title="MAKE IT FISHEYE" />
+        <CreationCard styles="bg-smAbove md:bg-lgAbove" title="FROM UP ABOVE VR" />
+        <CreationCard styles="bg-smCuriosity md:bg-lgCuriosity" title="THE CURIOSITY" />
+        <CreationCard styles="bg-smPocket md:bg-lgPocket" title="POCKET BOREALIS" />
+        <CreationCard styles="bg-smFisheye md:bg-lgFisheye" title="MAKE IT FISHEYE" />
       </div>
 
       <button className="border tracking-widest font-semibold border-black w-40 h-10 mt-2">SEE ALL</button>
--- src/components/creations/creation-card/CreationCard.tsx
+++ src/components/creations/creation-card/CreationCard.tsx
@@ -1,14 +1,12 @@
-import React from "react"
 interface props {
-  image: string
+  styles: string
   title: string
 }
 
-const CreationCard: React.FC<props> = ({ image, title }) => {
+const CreationCard: React.FC<props> = ({ styles, title }) => {
   return (
-    <div className={`h-36 mb-6 relative`}>
-      <img className="absolute -z-10 w-full" src={image} alt="" />
-      <h3 className="text-[1.6rem] font-thin w-[40%] mt-12 ml-5 leading-6 text-white">{title}</h3>
+    <div className={`h-36 md:h-[30rem] mb-6 relative bg-no-repeat bg-cover ` + styles}>
+      <h3 className={"text-[1.6rem] font-thin w-[40%] mt-12 ml-5 leading-6 text-white"}>{title}</h3>
     </div>
   )
 }
