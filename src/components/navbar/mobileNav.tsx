ed26cbe52a6824f24cc512c477a1899102ec63b7
DundeeA <dundeedevelops@gmail.com>
2023-11-10 08:11:11 -0600

    Mapped mobile nav links, added animation

--- src/components/navbar/mobileNav.tsx
+++ src/components/navbar/mobileNav.tsx
@@ -2,34 +2,25 @@ import { useLockBodyScroll } from "@uidotdev/usehooks"
 
 function MobileNav() {
   useLockBodyScroll()
+  const navLinks = [
+    { text: "About", link: "#" },
+    { text: "Careers", link: "#" },
+    { text: "Events", link: "#" },
+    { text: "Products", link: "#" },
+    { text: "Support", link: "#" },
+  ]
+
   return (
     <div className="w-screen h-screen bg-black z-4  left-[0] top-[0] absolute z-3">
       <ul className="flex gap-[1.03rem] flex-col text-[1.4rem]  text-white tracking-[.15rem] mt-[56%] smd:mt-[25%] smd:pl-10 pl-6">
-        <li>
-          <a href="#" aria-label="About loop studio">
-            ABOUT
-          </a>
-        </li>
-        <li>
-          <a href="#" aria-label="Careers at loop studio">
-            CAREERS
-          </a>
-        </li>
-        <li>
-          <a href="#" aria-label="See events">
-            EVENTS
-          </a>
-        </li>
-        <li>
-          <a href="#" aria-label="See products">
-            PRODUCTS
-          </a>
-        </li>
-        <li>
-          <a href="#" aria-label="get support">
-            SUPPORT
-          </a>
-        </li>
+        {navLinks.map((link) => (
+          <li key={link.link} className={`relative animate-fade-right animate-duration-[1000ms]`}>
+            {" "}
+            <a href={link.link} aria-label={link.text}>
+              {link.text}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   )
